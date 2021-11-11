2ef0580aa5f55d728bacb8fdb902745db762e551
alenovich123@gmail.com <2783200Aa>
2021-11-11 18:04:30 +0500

    - fix

--- main.py
+++ main.py
@@ -3,13 +3,9 @@ import pygame
 
 
 def play(filename, windowname):
-    os.environ['SDL_VIDEO_WINDOW_POS'] = '%i,%i' % (0, 0)
-    os.environ['SDL_VIDEO_CENTERED'] = '0'
     pygame.display.set_caption(windowname)
-    screen = pygame.display.set_mode((64, 64))
-    screen.fill((0, 0, 1))
     video = VideoFileClip(filename)
-    video.preview()
+    video.resize((400, 400)).preview()
     mainLoop = True
     while mainLoop:
         for event in pygame.event.get():
@@ -23,5 +19,4 @@ def play(filename, windowname):
 if __name__ == '__main__':
     file_name = r"E:\\test_64_64.mp4"
     window_name = "window"
-    interframe_wait_ms = 30
     play(file_name, window_name)
